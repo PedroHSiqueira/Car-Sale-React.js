0a97b9437ef1daacd2b2eeb60aa946f76e2e2a5a
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-11-21 19:06:13 -0300

    ADD SweetAlert2

--- src/Components/Titulo.jsx
+++ src/Components/Titulo.jsx
@@ -1,10 +1,20 @@
+import Swal from "sweetalert2";
 
 function Titulo(){
+
+  function cardFavoritos(){
+    Swal.fire({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success"
+    });
+  }
+
   return (
     <div>
       <header className="bg-[#2e2e2e] pr-10 flex justify-between">
         <img src="./logo.png" alt="logo" />
-        <button className="hidden bg-zinc-600 my-10 px-2 text-white rounded-2xl tablet:block ">Favoritos</button>
+        <button onClick={cardFavoritos} className="hidden bg-zinc-600 my-10 px-2 text-white rounded-2xl tablet:block ">Favoritos</button>
       </header>
     </div>
   )
