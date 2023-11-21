a43b0c228dd27673b9e33456d60b97c2a6770e13
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-11-21 19:59:31 -0300

    style: Color changes

--- src/Components/Renderizacao.jsx
+++ src/Components/Renderizacao.jsx
@@ -15,19 +15,19 @@ function Renderizacao(props) {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <section className="bg-white w-11/12 flex flex-col items-center rounded-2xl shadow_image" >
+      <section className="bg-slate-100 w-11/12 flex flex-col items-center rounded-2xl shadow_image" >
         <img src={props.carro.imagem} alt="carro" className=" rounded-2xl shadow_image " />
 
         <div className="flex items-center gap-14 py-5">
           <h2 className="font-semibold text-xl">{props.carro.modelo}</h2>
-          <p>{props.carro.subtitulo}</p>
+          <p className="text-sm">{props.carro.subtitulo}</p>
         </div>
 
         <p className="text-3xl font-semibold">R$ {props.carro.preco.toLocaleString("pt-br", { minimumFractionDigits: 2 })}</p>
 
         <div className="flex items-center gap-14 my-5 ">
           <img src="./heart.png" className="cursor-pointer "/>
-          <button className="bg-zinc-500 p-2 text-white rounded-xl" onClick={() => cardFinanciamento(props)}>Financiamento</button>
+          <button className="bg-zinc-600 p-2 text-slate-50 border-4 border-amber-500 rounded-xl" onClick={() => cardFinanciamento(props)}>Financiamento</button>
           <p>{props.carro.ano}</p>
         </div>
       </section>
