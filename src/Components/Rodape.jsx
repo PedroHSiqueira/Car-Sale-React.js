de6970097f9f2def9300f20ffb4e16342b763cd9
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-11-21 18:01:28 -0300

    Card Width Change

--- src/Components/Rodape.jsx
+++ src/Components/Rodape.jsx
@@ -3,7 +3,7 @@ function Rodape(){
   return (
     <div>
       <footer className="bg-[#2e2e2e] p-5">
-        <p className="text-white text-center"> © 1995-2023 Milestone Motor. Todos os direitos reservados.</p>
+        <p className="text-white text-center"> © 2023-2024 Milestone Motor. Todos os direitos reservados.</p>
       </footer>
     </div>
   )
