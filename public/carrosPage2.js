3e16c71f10236b3016e378b27e2e7edfd8edcd1e
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-11-22 17:34:14 -0300

    Feat: ADD more 12 cars to Page 12, Total: 15

--- public/carrosPage2.js
+++ public/carrosPage2.js
@@ -32,4 +32,136 @@ export const dados = [
     interior:
       "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231120/renault-kwid-1.0-12v-sce-flex-zen-manual-wmimagem15502638760.jpg?s=fill&w=1920&h=1440&q=75",
   },
+  {
+    id: 4,
+    modelo: "Volkswagen Kombi",
+    subtitulo: "1.4 LAST EDITION 8V",
+    preco: 72500,
+    ano: "2013/2014",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231027/volkswagen-kombi-1.4-last-edition-8v-flex-4p-manual-wmimagem08441688633.jpg?s=fill&w=1920&h=1440&q=75",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231027/volkswagen-kombi-1.4-last-edition-8v-flex-4p-manual-wmimagem08442508385.jpg?s=fill&w=1920&h=1440&q=75",
+  },
+  {
+    id: 5,
+    modelo: "Ford Ka",
+    subtitulo: "1.0 TI-VCT FLEX",
+    preco: 49900,
+    ano: "2019/2020",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231016/ford-ka-1.0-tivct-flex-se-manual-wmimagem10475401615.jpg?s=fill&w=1920&h=1440&q=75",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231016/ford-ka-1.0-tivct-flex-se-manual-wmimagem10475762389.jpg?s=fill&w=1920&h=1440&q=75",
+  },
+  {
+    id: 6,
+    modelo: "Mitsubishi Pajero",
+    subtitulo: "3.2 4X4 TURBO DIESEL",
+    preco: 157900,
+    ano: "2016/2016",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202309/20230914/mitsubishi-pajero-3.2-4x4-16v-turbo-intercooler-diesel-4p-automatico-wmimagem1215210064.jpg?s=fill&w=1920&h=1440&q=75",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202309/20230914/mitsubishi-pajero-3.2-4x4-16v-turbo-intercooler-diesel-4p-automatico-wmimagem12163619074.jpg?s=fill&w=1920&h=1440&q=75",
+  },
+  {
+    id: 7,
+    modelo: "Chevrolet Camaro",
+    subtitulo: "6.2 SS COUPÉ V8",
+    preco: 239000,
+    ano: "2013/2014",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202306/20230622/chevrolet-camaro-6.2-ss-coupe-v8-gasolina-2p-automatico-wmimagem20475791027.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202306/20230622/chevrolet-camaro-6.2-ss-coupe-v8-gasolina-2p-automatico-wmimagem20480524015.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 8,
+    modelo: "Nissan Frontier",
+    subtitulo: "2.3 16V TURBO DIESEL",
+    preco: 185000,
+    ano: "2020/2021",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231107/nissan-frontier-2.3-16v-turbo-diesel-le-cd-4x4-automatico-wmimagem22224192445.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231107/nissan-frontier-2.3-16v-turbo-diesel-le-cd-4x4-automatico-wmimagem22233658584.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 9,
+    modelo: "Ford Ranger",
+    subtitulo: "2.2 XLS 4X4 CD",
+    preco: 192900,
+    ano: "2022/2023",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231116/ford-ranger-2.2-xls-4x4-cd-16v-diesel-4p-automatico-wmimagem16412473975.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231116/ford-ranger-2.2-xls-4x4-cd-16v-diesel-4p-automatico-wmimagem16412588021.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 10,
+    modelo: "Hyundai Creta",
+    subtitulo: "1.0 TGDI FLEX",
+    preco: 114900,
+    ano: "2022/2022",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231115/hyundai-creta-1.0-tgdi-flex-platinum-automatico-wmimagem12061428433.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231115/hyundai-creta-1.0-tgdi-flex-platinum-automatico-wmimagem12061646022.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 11,
+    modelo: "Hyundai HB20",
+    subtitulo: "1.6 R SPEC 16V",
+    preco: 63900,
+    ano: "2016/2017",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231115/hyundai-hb20-1.6-r-spec-16v-flex-4p-automatico-wmimagem17242005762.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231115/hyundai-hb20-1.6-r-spec-16v-flex-4p-automatico-wmimagem17243574164.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 12,
+    modelo: "Fiat Toro",
+    subtitulo: "2.0 16V TURBO",
+    preco: 178000,
+    ano: "2022/2022",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231013/fiat-toro-2.0-16v-turbo-diesel-ultra-4wd-at9-wmimagem17592857621.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231013/fiat-toro-2.0-16v-turbo-diesel-ultra-4wd-at9-wmimagem17593736293.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 13,
+    modelo: "Ford Focus",
+    subtitulo: "2.0 TITANIUM 16V",
+    preco: 64490,
+    ano: "2016/2016",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231121/ford-focus-2.0-titanium-16v-flex-4p-powershift-wmimagem20312275838.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231121/ford-focus-2.0-titanium-16v-flex-4p-powershift-wmimagem20312763920.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 14,
+    modelo: "Nissan 370z",
+    subtitulo: "3.7 V6 GASOLINA",
+    preco: 429000,
+    ano: "2009/2009",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/nissan-370z-3.7-v6-gasolina-manual-wmimagem11500294634.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/nissan-370z-3.7-v6-gasolina-manual-wmimagem11500913950.jpg?s=fill&w=552&h=414&q=60",
+  },
+  {
+    id: 15,
+    modelo: "Volkswagen Golf",
+    subtitulo: "2.0 GTI HIGHLINE",
+    preco: 135990,
+    ano: "2014/2014",
+    imagem:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231117/volkswagen-golf-2.0-gti-highline-16v-gasolina-4p-automatico-wmimagem21231817781.jpg?s=fill&w=552&h=414&q=60",
+    interior:
+      "https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202311/20231117/volkswagen-golf-2.0-gti-highline-16v-gasolina-4p-automatico-wmimagem21232283812.jpg?s=fill&w=552&h=414&q=60",
+  },
 ];
